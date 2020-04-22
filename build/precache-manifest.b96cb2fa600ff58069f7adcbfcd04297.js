ee8df1a2ed3fd50268d9c8e0a79fce6ed70f468a
beemittens <lauri.i.soini@gmail.com>
2020-04-22 21:05:03 +0300

    Added deployment build of phone book

---
+++ build/precache-manifest.b96cb2fa600ff58069f7adcbfcd04297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abec575b837d32abeaee224c18106e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e79b229cf081a291da",
+    "url": "/static/css/main.10369450.chunk.css"
+  },
+  {
+    "revision": "f96d806b3f55726b8a94",
+    "url": "/static/js/2.40695959.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40695959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e79b229cf081a291da",
+    "url": "/static/js/main.0105a876.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
